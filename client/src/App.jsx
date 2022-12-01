1973d8050309abf3c02d1c1cd7795f8fc54b7ba4
vishnutvm <vishnuvichu8003@gmail.com>
2022-12-01 10:02:38 +0530

    redux basic setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,21 @@ import {BrowserRouter,Navigate,Routes,Route} from 'react-router-dom'
 import HomePage from 'pages/userPages/homePage/HomePage'
 import Loginpage from 'pages/userPages/loginPage/Loginpage'
 import ProfilePage from 'pages/userPages/profilePage/ProfilePage'
+import { useMemo } from 'react'
+import { useSelector } from 'react-redux'
+import { CssBaseline,ThemeProvider} from '@mui/material';
+import { createTheme } from '@mui/material'
+import { themeSettings } from 'theme'
 
 
 
 function App() {
   // const [count, setCount] = useState(0)
+  const mode = useSelector((state)=>state
+  .mode)
+  const theme = useMemo(()=>{
+    createTheme(themeSettings(mode))
+  },[mode])
 
   return (
 <div className='app'>
