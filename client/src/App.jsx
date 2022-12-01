2cbe0f2d13377915d7941283c8561cd75a8687ac
vishnutvm <vishnuvichu8003@gmail.com>
2022-12-01 10:44:41 +0530

    mui setup and theme,darkmood

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 import {BrowserRouter,Navigate,Routes,Route} from 'react-router-dom'
-import HomePage from 'pages/userPages/homePage/HomePage'
-import Loginpage from 'pages/userPages/loginPage/Loginpage'
-import ProfilePage from 'pages/userPages/profilePage/ProfilePage'
+import HomePage from './pages/userPages/homePage/HomePage.jsx'
+import Loginpage from './pages/userPages/loginPage/Loginpage.jsx'
+import ProfilePage from './pages/userPages/profilePage/ProfilePage.jsx'
 import { useMemo } from 'react'
 import { useSelector } from 'react-redux'
 import { CssBaseline,ThemeProvider} from '@mui/material';
 import { createTheme } from '@mui/material'
-import { themeSettings } from 'theme'
+import { themeSettings } from './theme.js'
 
 
 
@@ -16,21 +16,23 @@ function App() {
   // const [count, setCount] = useState(0)
   const mode = useSelector((state)=>state
   .mode)
-  const theme = useMemo(()=>{
-    createTheme(themeSettings(mode))
-  },[mode])
-
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
 <div className='app'>
 
 <BrowserRouter>
-<Routes>
+
+<ThemeProvider theme={theme}>
+  {/* <CssBaseline /> */}
+ <Routes>
 
 <Route  path='/' element={<Loginpage/>}/>
 <Route  path='/home' element={<HomePage/>}/>
 <Route  path='/profile/:userId' element={<ProfilePage/>}/>
 
-</Routes>
+</Routes> 
+</ThemeProvider>
+
 
 
 </BrowserRouter>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -34,7 +34,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistStore(store)}/>
-    </Provider>
     <App />
+    </Provider>
   </React.StrictMode>
 )
