ef4950a97824605d483f9fd0765696888ef9ff97
vishnutvm <vishnuvichu8003@gmail.com>
2022-12-01 16:42:31 +0530

    register and login page setup

--- client/src/pages/userPages/loginPage/Loginpage.jsx
+++ client/src/pages/userPages/loginPage/Loginpage.jsx
@@ -1,9 +1,128 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import './loginPage.css';
+import loginPageImage from './img/log.svg';
+import registerPageImage from './img/register.svg';
 
 const Loginpage = () => {
-  return (
-    <div>Loginpage</div>
-  )
-}
+  const container = useRef(null);
 
-export default Loginpage
+  const signin = () => {
+    console.log('sign in pressed');
+    container.current.classList.remove('sign-up-mode');
+  };
+
+  const signup = () => {
+    console.log('sign up pressed');
+    container.current.classList.add('sign-up-mode');
+  };
+
+  return (
+    <>
+      <div ref={container} className="container">
+        <div className="forms-container">
+          <div className="signin-signup">
+            <form action="#" className="sign-in-form">
+              <h2 className="title">Sign in</h2>
+              <div className="input-field">
+                <i className="fas fa-user" />
+                <input type="text" placeholder="Username" />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-lock" />
+                <input type="password" placeholder="Password" />
+              </div>
+
+              <input type="submit" defaultValue="Login" className="btn solid" />
+
+              <p className="social-text">Or Sign in with social platforms</p>
+              <div className="social-media">
+                <a href="#" className="social-icon">
+                  <i className="fab fa-facebook-f" />
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-twitter" />
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-google" />
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-linkedin-in" />
+                </a>
+              </div>
+            </form>
+            <form action="#" className="sign-up-form">
+              <h2 className="title">Sign up</h2>
+              <div className="input-field">
+                <i className="fas fa-user" />
+                <input type="text" placeholder="Username" />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-envelope" />
+                <input type="email" placeholder="Email" />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-lock" />
+                <input type="password" placeholder="Password" />
+              </div>
+              <input type="submit" className="btn" defaultValue="Sign up" />
+              <p className="social-text">Or Sign up with social platforms</p>
+              <div className="social-media">
+                <a href="#" className="social-icon">
+                  <i className="fab fa-facebook-f" />
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-twitter" />
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-google" />
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-linkedin-in" />
+                </a>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="panels-container">
+          <div className="panel left-panel">
+            <div className="content">
+              <h3>New here ?</h3>
+              <p>
+                Browse through an awesome collection of websites built by
+                developers across the globe and promote your projects as well.
+              </p>
+              <button
+                onClick={signup}
+                className="btn transparent"
+                id="sign-up-btn"
+              >
+                Sign up
+              </button>
+            </div>
+            <img src={loginPageImage} className="image" alt="" />
+          </div>
+          <div className="panel right-panel">
+            <div className="content">
+              <h3>One of us ?</h3>
+              <p>
+                Showcase your skills and projects and connect with other
+                developers across the globe. Lets grow Together
+              </p>
+              <button
+                onClick={signin}
+                className="btn transparent"
+                id="sign-in-btn"
+              >
+                Sign in
+              </button>
+            </div>
+            <img src={registerPageImage} className="image" alt="" />
+          </div>
+        </div>
+      </div>
+      ;
+    </>
+  );
+};
+
+export default Loginpage;
