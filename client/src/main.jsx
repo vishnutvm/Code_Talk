e96b2af0a70c386673e75f125024742d719a6569
vishnutvm <vishnuvichu8003@gmail.com>
2022-12-05 19:54:13 +0530

    store re-config

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import userAthReducer from './redux/userState/index'
-import { configureStore } from '@reduxjs/toolkit'
-import { Provider } from 'react-redux'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import userAthReducer from './redux/userState/index';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+
 // for local storage
-import { persistStore,persistReducer,FLUSH,PAUSE,REHYDRATE,PERSIST,PURGE,REGISTER } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
-import { PersistGate } from 'redux-persist/integration/react'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  PAUSE,
+  REHYDRATE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
 
+const persistConfig = { key: 'root', storage, version: 1 };
 
-
-
-
-
-const persistConfig = { key: "root", storage, version: 1 };
 const persistedReducer = persistReducer(persistConfig, userAthReducer);
+
 const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
@@ -29,12 +36,11 @@ const store = configureStore({
 
 
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistStore(store)}/>
-    <App />
+      <PersistGate loading={null} persistor={persistStore(store)} />
+      <App />
     </Provider>
   </React.StrictMode>
-)
+);
