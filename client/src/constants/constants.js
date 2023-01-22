2f915dbeaf4a8c73742c627ca74631fb5e71756d
vishnutvm <vishnuvichu8003@gmail.com>
2023-01-22 13:43:06 +0530

    added meeting with zegocloud small design issue remain

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/prefer-default-export */
 // base userl
 export const baseUrl = 'http://localhost:3001';
-export const appId = '710830467';
-export const serveSecret = '9323ce8b4028768b727dca5b7ec01435';
+export const appId = 1856343618;
+export const serveSecret = '44717af2fe435f9c4a36a6edb4b0459c';
--- client/src/pages/userPages/JoinMeet/Joinmeet.jsx
+++ client/src/pages/userPages/JoinMeet/Joinmeet.jsx
@@ -1,10 +1,17 @@
 import React, { useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+// import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
+
+import { useSelector } from 'react-redux';
 import axios from '../../../utils/axios';
+import { baseUrl, appId, serveSecret } from '../../../constants/constants';
 
 function Joinmeet() {
   const navigate = useNavigate();
   const { meetId } = useParams();
+
+  const { _id, username } = useSelector((state) => state.user.user);
   const CheckUser = () => {
     axios
       .get(`/user/getMeet/${meetId}`)
@@ -24,10 +31,43 @@ function Joinmeet() {
     CheckUser();
   }, []);
 
-// joining the meet
+  // joining the meet using link, adding zegocloud
+  const myMeeting = async (element) => {
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appId,
+      serveSecret,
+      meetId,
+      _id,
+      username
+    );
+    console.log(kitToken);
 
+    // Create instance object from Kit Token.
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
 
-  return <h1>Joining meeting</h1>;
+    zp.joinRoom({
+      container: element,
+      sharedLinks: [
+        {
+          name: 'Personal link',
+          url: window.location.origin,
+        },
+      ],
+      scenario: {
+        mode: ZegoUIKitPrebuilt.VideoConference,
+      },
+    });
+  };
+
+  return (
+    <div className="wrapper">
+      <div
+        className="myCallContainer "
+        ref={myMeeting}
+        style={{ width: '100vw', height: '100vh' }}
+      />
+    </div>
+  );
 }
 
 export default Joinmeet;
